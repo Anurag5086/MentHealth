562a4b7af5da0d69878eeacd873a8cc7906cd6df
MAHESH SINGH <richa.singhnxt213@gmail.com>
2022-10-11 20:34:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+function fun(){
+    let state=document.getElementById("country-state").value
+    if(state=="AS")
+    {
+        document.getElementById("Assam").scrollIntoView()
+    }
+    else if(state=="CN"){
+        document.getElementById("Chandigarh").scrollIntoView()
+    }
+    else if(state=="CT"){
+        document.getElementById("Chhattisgarh").scrollIntoView()
+    }
+    
+    else if(state=="DL"){
+        document.getElementById("Delhi").scrollIntoView()
+    }
+    else if(state=="GA"){
+        document.getElementById("Goa").scrollIntoView()
+    }
+    else if(state=="GJ"){
+        document.getElementById("Gujarat").scrollIntoView()
+    }
+    else if(state=="JH"){
+        document.getElementById("Jharkhand").scrollIntoView()
+    }
+    else if(state=="JK"){
+        document.getElementById("JandK").scrollIntoView()
+    }
+    else if(state=="KA"){
+        document.getElementById("Karnataka").scrollIntoView()
+    }
+    else if(state=="KL"){
+        document.getElementById("Kerala").scrollIntoView()
+    }
+    else if(state=="AN"){
+        document.getElementById("MP").scrollIntoView()
+    }
+    else if(state=="MP"){
+        document.getElementById("Maharashtra").scrollIntoView()
+    }
+    else if(state=="OR"){
+        document.getElementById("Odisha").scrollIntoView()
+    }
+    else if(state=="PY"){
+        document.getElementById("Pondicherry").scrollIntoView()
+    }
+    else if(state=="PB"){
+        document.getElementById("Punjab").scrollIntoView()
+    }
+    else if(state=="RJ"){
+        document.getElementById("Rajasthan").scrollIntoView()
+    }
+    else if(state=="SK"){
+        document.getElementById("Sikkim").scrollIntoView()
+    }
+    else if(state=="TN"){
+        document.getElementById("TN").scrollIntoView()
+    }
+    else if(state=="TG"){
+        document.getElementById("Telangana").scrollIntoView()
+    }
+    else if(state=="WB"){
+        document.getElementById("WB").scrollIntoView()
+    }
+    }
